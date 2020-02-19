ea75bffd1e2dbb535be0c45b5513e86e49bdb94a
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-02-19 23:36:24 +0700

    Создание сборки Vue приложения

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+
+}
+p {
+  a {
+    color: $main-color;
+  }
+}
+a {
+  color: #42b983;
+}
+</style>
