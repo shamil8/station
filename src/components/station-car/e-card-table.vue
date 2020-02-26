16530adca18838b2c6367e856fbe7ee6dd3717b3
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-02-26 10:36:09 +0700

    Добавлено описания проекта

--- src/components/station-car/e-card-table.vue
+++ src/components/station-car/e-card-table.vue
@@ -69,7 +69,7 @@
         data() {
             return {
                 loading: false,
-                total: 1,
+                total: 0,
                 pageSize: 3
             }
         },
